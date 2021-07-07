c1fac8e98d9a4e0f5bd718faa51e5c10b007ff6d
Samantha Yuhan <sysweettooth1@gmail.com>
2021-07-07 00:01:01 -0700

    get all info from API and start save history function

--- script.js
+++ script.js
@@ -20,25 +20,49 @@ function getWeather(name) {
     axios.get(queryURL)
     .then(function(response){
         var weatherPic = response.data.weather[0].icon;
-        var lat = response.data.coord.lat;
-        var lon = response.data.coord.lon;
-        var UVURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+        var latitude = response.data.coord.lat;
+        var longitude = response.data.coord.lon;
+        var UVURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&cnt=1";
 
         // Add date to weather info using Moment.js
         cityEl.innerHTML = response.data.name + " (" + moment().format('l') + ")";
         // Get the weather info from API
         weatherEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
-        tempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+        tempEl.innerHTML = "Temperature: " + convertTemp(response.data.main.temp) + " &#176F";
         humidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
         windEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
-        
         axios.get(UVURL)
             
+        // Get UV index
+        .then(function(response){
+            var UV = document.createElement("span");
+            UV.setAttribute("class","badge badge-danger");
+            UV.innerHTML = response.data[0].value;
+            UVEl.innerHTML = "UV Index: ";
+            UVEl.append(UV);
+        });
+
         var city = response.data.id;
         var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + city + "&appid=" + APIKey;
         axios.get(forecastURL)
+        
+        // Get forecast for next 5 days
+        .then(function(response){
             
+        })
     });  
 }
     
-        
+// Create event listener to save cities that were inputted
+searchBtnEl.addEventListener("click",function() {
+    var input = inputEl.value;
+    getWeather(input);
+    savedHistory.push(input);
+    localStorage.setItem("search", JSON.stringify(savedHistory));
+    // Call function for search history
+})
+    
+function convertTemp(t) {
+    return Math.floor(1.8 * (t - 273.15) + 32);
+}
+    
